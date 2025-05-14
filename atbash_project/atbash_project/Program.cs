799d29fd19a281994f4ff9cadbcf5c0274b4bd41
ezriel <ezrirot@gmail.com>
2025-05-14 19:13:13 +0300

    level  of danger function (1-3)

--- atbash_project/atbash_project/Program.cs
+++ atbash_project/atbash_project/Program.cs
@@ -19,6 +19,35 @@
         }
 
 
+        static string LevelOfDanger(int score) 
+            {
+            
+            int rank = 0;
+            if (score > 0 && score <= 5)
+            {
+                rank = 1;
+            }
+            else if (score > 0 && score >= 5 && score <= 10)
+            {
+                rank = 2;
+            }
+            else if(score > 0)
+            {
+                rank = 3;
+            }
+            switch (score)
+            {
+                case 1:
+                    return "WARNING";
+                case 2:
+                    return "DANGER!";
+                case 3:
+                   return "ULTRA ALERT!";
+            }
+            return "not correct input please try again";
+
+            }
+
         static void Main(string[] args)
         {
 
