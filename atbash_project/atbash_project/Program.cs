05c63d3d665bd210fd48006021dd4e1a831ce253
ezriel <ezrirot@gmail.com>
2025-05-14 17:50:24 +0300

    first version decryption function

---
+++ atbash_project/atbash_project/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace atbash_project
+{
+    internal class Program
+    {
+      static string decryptAtbash(string text)
+        {
+            string decryptTxt = "";
+            var DecryptionKey = new Dictionary<char, char>() { { 'a', 'z' }, { 'b', 'y' }, { 'c', 'x' }, { 'd', 'w' }, { 'e', 'v' }, { 'f', 'u' }, { 'g', 't' }, { 'h', 's' }, { 'i', 'r' }, { 'j', 'q' }, { 'k', 'p' }, { 'l', 'o' }, { 'm', 'n' } };
+            for(int i =0; i< text.Length;i++) 
+            {
+                
+                if (char.IsLetter(text[i]))
+                {
+                    decryptTxt += DecryptionKey[char.ToLower(text[i])];
+                }
+           
+            }
+            return decryptTxt;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+        }
+    }
+}
